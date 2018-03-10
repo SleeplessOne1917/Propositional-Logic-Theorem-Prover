1e9180170bd9fdc2a588b0cb90d2b2e649034371
Anthony Bias <abias1122@gmail.com>
2018-03-10 12:34:41 -0500

    Make function for bringing in negation.

--- theorem-prover.lisp
+++ theorem-prover.lisp
@@ -27,19 +27,19 @@
   "Return true if an atom is an atom or the
 negation of an atom."
   (or (atom-p prop) (and (negation-p prop) 
-			 (atom-p (second prop)))))
+			 (atom-p (negation-operand prop)))))
 
 (defun conjunction-p (prop)
   "Retrun true if the proposition is a conjunction."
-  (and (listp prop) (eq ^ (second prop))))
+  (and (listp prop) (eq ^ (connector prop))))
 
 (defun disjunction-p (prop)
   "Retrun true if the proposition is a conjunction."
-  (and (listp prop) (eq v (second prop))))
+  (and (listp prop) (eq v (connector prop))))
 
 (defun negation-p (prop)
   "Return true if prop is a negation."
-  (and (listp prop) (eq (first prop) ~)))
+  (and (listp prop) (eq (negation-sign prop) ~)))
 
 ;;;Selectors
 (defun connector (prop)
@@ -58,45 +58,46 @@ negation of an atom."
   (second prop))
 
 ;;;Functions for putting into CNF
-(defun bring-in-negation (prop &optional (has-even-negs nil))
+(defun bring-in-negation (prop &optional (has-even-negs nil negs-supplied-p))
   "Simplify a negation by eliminating extra negations signs
 and applying DeMorgan's law to conjunctions and disjunctions."
   (flet ((double-negation-p (p)
 	   (eq (negation-operand p) ~)))
-    (cond ((disjunction-p (negation-operand prop))                   ;prop is a disjunction
-	   `((~ ,(prop1 prop)) ^ (~ ,(prop2 prop))))
-	  ((conjunction-p (negation-operand prop))                   ;prop is a conjunction
-	   `((~ ,(prop1 prop)) v (~ ,(prop2 prop))))
-	  (has-even-negs (if (double-negation-p prop)                ;prop is negation with even # of negation symbols
-			  (bring-in-negation (rest prop) nil)
-			  (negation-operand prop)))
-	  (t (if (double-negation-p prop)                            ;prop is a negation with an odd # of negation symbols
-		 (bring-in-negation (rest prop) t)
-		 `(~ ,(negation-operand prop)))))))
+    (let ((operand (negation-operand prop)))
+      (macrolet ((double-neg-bring-in (p else &optional (even-negs nil))
+		   `(if (double-negation-p ,p)
+			(bring-in-negation (rest ,p) ,even-negs)
+			,else)))
+      (cond 
+	(negs-supplied-p (if has-even-negs 
+			     (double-neg-bring-in prop operand nil)              ;prop is negation with even # of negation symbols
+			     (double-neg-bring-in  prop (negate operand) t)))                      ;prop is a negation with an odd # of negation symbols		 
+	((disjunction-p operand)                   ;prop is a disjunction
+	 `(,(negate (prop1 operand)) ^ ,(negate (prop2 operand))))
+	((conjunction-p operand)                   ;prop is a conjunction
+	 `(,(negate (prop1 operand)) v ,(negate (prop2 operand))))
+	(t (double-neg-bring-in prop (negate operand) t)))))))
 
 (defun distribute-or (prop)
   "Distribute disjunction; e.g. (P v (Q ^ R))
 becomes (P v Q) ^ (P v R). Assumes prop is a disjunction
 and at least one of the sub propositions is a conjunction."
-  (let ((prop1 (prop1 prop))
-	(prop2 (prop2 prop)))
-    (if (conjunction-p prop1)
-	`((,(prop1 prop1) v ,prop2) ^ (,(prop2 prop1) v ,prop2))
-	`((,(prop1 prop2) v ,prop1) ^ (,(prop2 prop2) v ,prop1)))))
+  (with-props (prop p1 p2)
+    (if (conjunction-p p1)
+	`((,(prop1 p1) v ,p2) ^ (,(prop2 p1) v ,p2))
+	`((,(prop1 p2) v ,p1) ^ (,(prop2 p2) v ,p1)))))
 
 (defun expand-implication (prop)
   "Convert proposition of the form P -> Q to
 ~P v Q. Assumes prop is a implication."
-  (let ((prop1 (prop1 prop))
-	(prop2 (prop2 prop)))
-      `(,(negate prop1) v ,prop2)))
+  (with-props (prop p1 p2) 
+    `(,(negate p1) v ,p2)))
 
 (defun expand-biconditional (prop)
   "Convert proposition of the form P <-> Q
 to ((~ P) v Q) ^ ((~ Q) v P). Assumes prop is a biconditional."
-  (let ((prop1 (prop1 prop))
-	(prop2 (prop2 prop)))
-    `((,(negate prop1) v ,prop2) ^ (,(negate prop2) v ,prop1))))
+  (with-props (prop p1 p2)
+    `((,(negate p1) v ,p2) ^ (,(negate p2) v ,p1))))
 
 (defun negate (prop)
   "Negate a proposition."
@@ -105,6 +106,7 @@ to ((~ P) v Q) ^ ((~ Q) v P). Assumes prop is a biconditional."
       `(~ ,prop)))
 
 ;;;UTILITY
-(defmacro with-gensyms ((&rest names) &body body)
-  `(let ,(loop for n in names collect `(,n (gensym)))
+(defmacro with-props ((prop prop1 prop2) &body body)
+  `(let* ((,prop1 (prop1 ,prop))
+	  (,prop2 (prop2 ,prop)))
      ,@body))
